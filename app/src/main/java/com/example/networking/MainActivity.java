258f8b1b2502b5545929df30787bf0c6c483a24a
g21emmka <g21emmka@student.his.se>
2022-05-05 13:46:29 +0200

    created a ArrayList

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -6,11 +6,20 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
 
     RecyclerView recyclerView;
+
+    ArrayList<MyViewHolder> items = new ArrayList<>(Arrays.asList(
+            new MyViewHolder("Matterhorn"),
+            new MyViewHolder("Mont Blanc"),
+            new MyViewHolder("Denali")
+    ));
 
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
--- app/src/main/java/com/example/networking/MyViewHolder.java
+++ app/src/main/java/com/example/networking/MyViewHolder.java
@@ -14,4 +14,4 @@ public class MyViewHolder {
             return title;
         }
     }
-}
+
