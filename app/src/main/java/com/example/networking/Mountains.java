be464dee83d0eab7b4485befba337654cd748a47
g21emmka <g21emmka@student.his.se>
2022-05-06 14:35:26 +0200

    Added strings and int in Mountains and added a new class

--- app/src/main/java/com/example/networking/Mountains.java
+++ app/src/main/java/com/example/networking/Mountains.java
@@ -6,12 +6,25 @@ package com.example.networking;
 "type":"brom",
 "company":"",
 "location":"The Karakoram range",
-"category":"","size":8611,"cost":28251,
-"auxdata":{"wiki":"https://en.wikipedia.org/wiki/K2",
+"category":"",
+"size":8611,
+"cost":28251,
+"auxdata":{
+"wiki":"https://en.wikipedia.org/wiki/K2",
 "img":"https://en.wikipedia.org/wiki/K2#/media/File:K2_2006b.jpg"},
  */
 
 public class Mountains {
 
     private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+    private String location;
+    private String size;
+    private int cost;
+    private int location;
+    private Auxdata auxdata;
 }
